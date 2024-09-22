12285c4c08c2b1577ca14cc433b69e81911ba720
Abdullah Khan <139475439+Abdullah-K13@users.noreply.github.com>
2024-09-22 20:14:36 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 
 # Initialize the producer
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',  # Adjust if necessary
+    bootstrap_servers='localhost:9092,localhost:9093',  # Adjust if necessary
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),  # Serialize JSON
     key_serializer=lambda k: str(k).encode('utf-8')  # Serialize keys
 )
